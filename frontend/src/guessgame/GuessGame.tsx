aad94c614109b37fd6af08eb69ebb92b2b266a06
Eric Huie <erichuie97@gmail.com>
2024-04-11 23:43:48 -0700

    added gfeature of getting all game screensho withh left and right carousel arrows to navigate

--- frontend/src/guessgame/GuessGame.tsx
+++ frontend/src/guessgame/GuessGame.tsx
@@ -4,15 +4,25 @@ import LoadingSpinner from "../common/LoadingSpinner";
 function GuessGame() {
   const [gameImage, setGameImage] = useState(
     {
-      "images": [],
+      "images": [""],
     }
   );
+
+  const [currImgIdx, setCurrImgIdx] = useState(0);
+
+  let total = gameImage["images"].length;
+  console.log("total", total);
+  const hideLeftArrow = currImgIdx === 0 ? "hidden" : "";
+  const hideRightArrow = currImgIdx === total - 1 ? "hidden" : "";
 
   async function getGame() {
     const response = await fetch("http://localhost:5000/new-game");
     const gameResults = await response.json();
     console.log("test output of api request", gameResults);
     setGameImage(gameResults);
+    // setGameImage({
+    //   "images": ["/assets/images/testImg.jpg"]
+    // });
   }
 
 
@@ -21,13 +31,35 @@ function GuessGame() {
     getGame();
   }, []);
 
+  //Increments currImgIdx state by 1
+  function goForward() {
+    if(currImgIdx < gameImage["images"].length - 1) setCurrImgIdx(currImgIdx + 1);
+  }
+
+  //Decrements currImgIdx state by 1
+  function goBackward(){
+    if(currImgIdx > 0) setCurrImgIdx(currImgIdx - 1);
+  }
+
   if (gameImage["images"].length === 0) return <LoadingSpinner />;
+
 
   return (
     <div>
-      {gameImage["images"].map((image, index) =>
+      <i
+        className={`bi bi-arrow-left-circle ${hideLeftArrow}`}
+        onClick={goBackward}
+      />
+
+      {/* {gameImage["images"].map((image, index) =>
         <img key={index} src={image} alt="no image" width="500" height="600" />
-      )}
+      )} */}
+      <img src={gameImage["images"][currImgIdx]} alt="no image" width="600" height="600" />
+
+      <i
+        className={`bi bi-arrow-right-circle ${hideRightArrow}`}
+        onClick={goForward}
+      />
     </div>
   );
 }
